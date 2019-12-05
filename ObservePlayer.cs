0a304cc4e182ad6e4ed4a1c37a5e66f81bdcd7fa
cjpmaubry <cjpm.aubry@gmail.com>
2019-12-05 13:21:26 +0100

    Add ObservePlayer (the observer of player class)

---
+++ ObservePlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly_2019
+{
+    public class ObservePlayer : IObserver
+    {
+
+        private string name;
+
+
+        // Constructor
+
+        public ObservePlayer(string name)
+        {
+            this.name = name;
+        }
+
+
+        /// <summary>
+        /// Display the information of position for the player notify
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="name"></param>
+        public void UpdatePosition(int position, string name)
+        {
+            Console.WriteLine("Le joueur " + name + " est maintenant sur la case numero " + position);
+        }
+
+        /// <summary>
+        ///  Display the information of money for the player notify
+        /// </summary>
+        /// <param name="money"></param>
+        /// <param name="name"></param>
+        public void UpdateMoney(double money, string name)
+        {
+            Console.WriteLine("Le joueur " + name + " à maintenant " + money + " euro");
+        }
+
+    }
+}
