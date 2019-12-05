509e3bd2819aac651dad826455413ae33adf3543
Leopold DUVERGER-NOINSKI <leopold.duvergernoinski@edu.devinci.fr>
2019-12-05 13:53:19 +0100

    Factory class allowing to create different boxs

---
+++ BoxFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly_2019
+{
+    class BoxFactory
+    {
+        //creates a Jail box at the given position
+        public static void createJail(int position, Abs_Box[] plateau)
+        {
+            plateau[position] = new Jail(position);
+        }
+
+        //creates a GoToJail box at the given position
+        public static void createGoToJail(int position, Abs_Box[] plateau)
+        {
+            plateau[position] = new GoToJail(position);
+        }
+
+        //created a Neutral box at the given position
+        public static void createNeutral(int position, Abs_Box[] plateau)
+        {
+            plateau[position] = new Neutral(position);
+        }
+    }
+}
