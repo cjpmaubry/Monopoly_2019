f6d10b356cba760773d3e78b4632cc0a73bf464c
Jérémy GOLDSCHILD <goldschildj@gmail.com>
2019-12-05 13:33:32 +0100

    Add Dice class

---
+++ Dice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly_2019
+{
+    class Dice
+    {
+        int[] dices;
+
+        public Dice(int[] dices)
+        {
+            this.dices = dices;
+        }
+
+        //function which returns an array with two numbers randomly generated between 1 and 6
+        public static int[] RollDice(int[] dice)
+        {
+            Random alea = new Random();
+            for (int i = 0; i < dice.Length; i++)
+                dice[i] = alea.Next(6) + 1;
+            return dice;
+        }
+
+        //Funcion that calaculate the sum of the dices
+        public static int SumDice(int[] dice)
+        {
+            int res = dice[0] + dice[1];
+            return res;
+        }
+
+        //Function which checks if the dices are equalss
+        public static Boolean DoubleDice(int[] dice)
+        {
+            bool doubledice = false;
+            if (dice[0] == dice[1])
+            {
+                doubledice = true;
+            }
+            return doubledice;
+        }
+    }
+}
