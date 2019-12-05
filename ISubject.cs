402fdcab2bfc9e6895776a209dc6d2b904a08fa3
cjpmaubry <cjpm.aubry@gmail.com>
2019-12-05 13:18:27 +0100

    Add interface for the observer

---
+++ ISubject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly_2019
+{
+    interface ISubject
+    {
+        void Attach(ObservePlayer observer);
+        void Detach(ObservePlayer observer);
+        void NotifyMoney();
+        void NotifyPosition();
+    }
+}
