f070d2694a2ea49cbfc033e8901c0f43b9da71a0
Leopold DUVERGER-NOINSKI <leopold.duvergernoinski@edu.devinci.fr>
2019-12-14 17:53:35 +0100

    Added comments

--- BoxFactory.cs
+++ BoxFactory.cs
@@ -5,68 +5,82 @@ using System.Text;
 namespace Monopoly_2019
 {
     class BoxFactory
-    {     
-        //creates a Go box at the given position
+    {
+        /// <summary>
+        /// Creates a Go box at the given position
+        /// </summary>
         public static void createGo(int position, Abs_Box[] plateau)
         {
             plateau[position] = new Go(position);
         }
 
-        //creates a Proprety at the given position
+        /// <summary>
+        /// Creates a Proprety at the given position
+        /// </summary>
         public static void createProprety(int position, Abs_Box[] plateau, string name, int price, int rent, System.ConsoleColor color)
         {
             plateau[position] = new Proprety(position, name, price, rent, color);
         }
 
-        //creates a Utility at the given position
+        /// <summary>
+        /// Creates a Utility at the given position
+        /// </summary>
         public static void createUtility(int position, Abs_Box[] plateau, string name)
         {
             plateau[position] = new Utility(position, name);
         }
 
-        //creates a Railroad at the given position
+        /// <summary>
+        /// Creates a Railroad at the given position
+        /// </summary>
         public static void createRailroad(int position, Abs_Box[] plateau, string name)
         {
             plateau[position] = new Railroad(position, name);
         }
 
-        //creates a Luck box at given position
+        /// <summary>
+        /// Creates a Luck box at given position
+        /// </summary>
         public static void createLuck(int position, Abs_Box[] plateau)
         {
             plateau[position] = new Luck(position);
         }
 
-        //creates the FreeParking box
+        /// <summary>
+        /// Creates a Free Parking box at the given position
+        /// </summary>
         public static void createFreeParking(int position, Abs_Box[] plateau)
         {
             plateau[position] = new FreeParking(position);
         }
 
-        //creates a Jail box at the given position
+        /// <summary>
+        /// Creates a Jail box at the given position
+        /// </summary>
         public static void createJail(int position, Abs_Box[] plateau)
         {
             plateau[position] = new Jail(position);
         }
 
-        //creates a GoToJail box at the given position
+        /// <summary>
+        /// Creates a GoToJail box at the given position
+        /// </summary>
         public static void createGoToJail(int position, Abs_Box[] plateau)
         {
             plateau[position] = new GoToJail(position);
         }
 
-        //created a Neutral box at the given position
-        public static void createNeutral(int position, Abs_Box[] plateau)
-        {
-            plateau[position] = new Neutral(position);
-        }
-
-        //creates a Community chest  box at given position
+        /// <summary>
+        /// Creates a Community Chest box at given position
+        /// </summary>
         public static void createCommunityChest(int position, Abs_Box[] plateau)
         {
             plateau[position] = new CommunityChest(position);
         }
 
-        //creates a Tax box at given position
+        /// <summary>
+        /// Creates a Tax box at given position
+        /// </summary>
         public static void createTax(int position, Abs_Box[] plateau, string name)
         {
             plateau[position] = new Tax(position, name);
